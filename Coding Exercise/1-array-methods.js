67f6acc7b2e7061d94cea41d39c36d6b9f33777b
95mich <1995ichsan@gmail.com>
2023-02-02 10:40:00 +0700

    add file

--- Coding Exercise/1-array-methods.js
+++
@@ -1,7 +0,0 @@
-function transformToObjects(numberArray) {
-    // Todo: Add your logic
-    // should return an array of objects
-    return numberArray.map(number => {
-        return {val: number}
-    });
-}
